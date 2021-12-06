02540d29ce8a32494b7b807c048a05395a98aa02
Bence A. Tóth <tothab@gmail.com>
2021-12-06 18:46:07 +0100

    Added unit tests for shuffleArray

---
+++ components/utility.test.js
@@ -0,0 +1,34 @@
+import { shuffleArray } from "./utility";
+
+describe("shuffleArray", () => {
+  it("should return an array of the same size", () => {
+    const originalArray = [1, 2, 3, 4, 5];
+    const shuffledArray = shuffleArray(originalArray);
+    expect(originalArray.length).toEqual(shuffledArray.length);
+  });
+
+  it("should contain all the elements of the original array", () => {
+    const originalArray = [1, 2, 3, 4, 5];
+    const shuffledArray = shuffleArray(originalArray);
+    originalArray.forEach((element) => {
+      expect(shuffledArray).toContain(element);
+    });
+  });
+
+  it("should preserve duplicates", () => {
+    const originalArray = [1, 1, 2, 2, 3, 3, 3];
+    const shuffledArray = shuffleArray(originalArray);
+    expect(shuffledArray.filter((element) => element === 1)).toHaveLength(2);
+    expect(shuffledArray.filter((element) => element === 2)).toHaveLength(2);
+    expect(shuffledArray.filter((element) => element === 3)).toHaveLength(3);
+  });
+
+  it("should work with huge arrays", () => {
+    const originalArray = [...Array(10000).keys()];
+    const shuffledArray = shuffleArray(originalArray);
+    expect(originalArray.length).toEqual(shuffledArray.length);
+    originalArray.forEach((element) => {
+      expect(shuffledArray).toContain(element);
+    });
+  });
+});
